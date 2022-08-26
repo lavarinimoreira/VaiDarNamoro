0ab5e8bd5de750cf97e3fc7429c6e72b53be9d06
Gabriel Lavarini <lavarinimoreira@gmail.com>
2022-08-25 23:08:22 -0300

    Adding the relation of the click of the button with the audio

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
-function playGostei(){
-    document.querySelector('#som_tecla_gostei').play();
+
+function reproduzAudio(idElementAudio, /*fraseDoAudio*/){
+    document.querySelector(idElementAudio).play();
+    //document.querySelector('h1').innerHTML = fraseDoAudio
 }
 
-document.querySelector('.tecla_gostei').onclick = playGostei;
+const LISTA_DOS_AUDIOS = document.querySelectorAll('.tecla');
 
+let contador = 0;
+let quantidadeElementosNaLista = LISTA_DOS_AUDIOS.length;
+
+while(contador < quantidadeElementosNaLista){
+    
+    const TECLA = LISTA_DOS_AUDIOS[contador];
+    const AUDIO = TECLA.classList[1];
+
+    // template string
+    const ID_AUDIO = `#som_${AUDIO}`;
+    
+    TECLA.onclick = function(){
+        reproduzAudio(ID_AUDIO);
+    };
+
+    contador++;
+}
