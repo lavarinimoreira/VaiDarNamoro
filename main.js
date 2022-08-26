1606ed11208966c1f4166cde7ca4afc0e43e3078
Gabriel Lavarini <lavarinimoreira@gmail.com>
2022-08-26 17:54:50 -0300

    Adding a list with the words of the buttons

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
-
-function reproduzAudio(idElementAudio, /*fraseDoAudio*/){
+function reproduzAudio(idElementAudio, fraseDoAudio){
     document.querySelector(idElementAudio).play();
-    //document.querySelector('h1').innerHTML = fraseDoAudio
+    document.querySelector('h1').innerHTML = fraseDoAudio
 }
 
 const LISTA_DOS_AUDIOS = document.querySelectorAll('.tecla');
+let frasesDosBotoes = ["<center>Ai...<br>--- GOSTEI ---",
+                        "<center>Que isso meu filho...<br>--- CALMA ---",
+                        "<center>Dança Gatinho<br> ---- DANÇA ----",
+                        "--- DEMAAAIX ---",
+                        "**** C A V A L O ****",
+                        "iiIIIiIIIIRRA",
+                        "<- Ele gOoOosta ->",
+                        "~ PARE ~",
+                        "- - - UUUUUUUIIIIR - - -"
+];
 
 let quantidadeElementosNaLista = LISTA_DOS_AUDIOS.length;
 
@@ -16,6 +25,13 @@ for(let contador = 0; contador < quantidadeElementosNaLista; contador++){
     const ID_AUDIO = `#som_${AUDIO}`; // template string
     
     SEGUNDA_CLASSE_DA_TECLA.onclick = function(){
-        reproduzAudio(ID_AUDIO);
+        reproduzAudio(ID_AUDIO, frasesDosBotoes[contador]);
     }
+
+    /*
+    SEGUNDA_CLASSE_DA_TECLA.onkeydown = function(){
+        SEGUNDA_CLASSE_DA_TECLA.classList.add('ativa');
+    }
+    */
+
 }
